5b26f4569061be0b8b7dc6f75ed443c4b5590627
anonymousheadlights <62854004+anonymousheadlights@users.noreply.github.com>
2020-04-21 15:45:19 -0400

    Update Event.java

--- java/mains/Event.java
+++ java/mains/Event.java
@@ -4,7 +4,13 @@ public class Event {
 
     public String name, building, room, time, description;
 
-    public Event() {};
+    public Event() {}
+    public Event(String name, String building, String room, String time) {
+        this.name = name;
+        this.building = building;
+        this.room = room;
+        this.time = time;
+    }
     public Event(String name, String building, String room, String time, String description) {
         this.name = name;
         this.building = building;
@@ -12,4 +18,12 @@ public class Event {
         this.time = time;
         this.description = description;
     }
+
+    public String getName(){return name;}
+    public String getBuilding(){return building;}
+    public String getRoom(){return room;}
+    public String getTime(){return time;}
+    public String getDescript(){return description;}
+
+    //database stuff here?
 }
