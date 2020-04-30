e06d732d5b7e21b0778568e94611f6e696fc2e07
anonymousheadlights <62854004+anonymousheadlights@users.noreply.github.com>
2020-04-30 19:23:09 -0400

    Update HomePage.java

--- java/mains/HomePage.java
+++ java/mains/HomePage.java
@@ -51,38 +51,86 @@ public class MainActivity extends AppCompatActivity {
 
         goToCalen  = findViewById(R.id.goToCalen);
 
-        //emptyView = findViewById(android.R.id.empty);
+        //LIZZIE - CHANGES FROM 4/30/2020 START HERE -----------------------------------------------
+
+        //ACCEPTING ARRAYLIST FROM FUNCTION - TESTING ARRAYLIST
+        //ArrayList that will hold events
+
+        //Place function (DayEvent) here that would get the ArrayList for
+        //the specific date.
+            //todayEvents represents that ArrayList that would be
+            //returned by the function (DayEvent).
 
         //Create ArrayList list
-        ArrayList<String> todayArray = new ArrayList<>();
+        //NOTICE - ArrayList are set as String. I am not sure if you would need to change that
+        ArrayList<String> todayArray;
 
-        todayArray.add("Test List Item");
-        todayArray.add("Second List Item");
+        ArrayList<String> todayEvents = new ArrayList<>();
+
+
         
+        //Testing to ensure the method works, scrolling works, and long messages work
+            //Changed row_item.xml so that the listitem's height is wrap_content
+        todayEvents.add("Test List Item");
+        todayEvents.add("Second List Item");
+        todayEvents.add("Long List Item that wil test how long this message can be displayed");
+        todayEvents.add("Testing Scroll1");
+        todayEvents.add("Testing Scroll2");
+        todayEvents.add("Testing Scroll3");
+        todayEvents.add("Testing Scroll4");
+        todayEvents.add("Testing Scroll5");
+
+        //The EmptyView still works with this method
+        //of setting the ArrayList
+
+        todayArray = new ArrayList<>(todayEvents);
+        //todayArray.addAll(todayEvents);
+
+        //I kept the todayArray as it is the ArrayList used in the adapter.
+        //In theory, you should be able to place the DayEvent in the constructor
+        //since it returns an ArrayList.
+            //If not, I left the commented addAll method
+            //in case that does not work.
+        
+        //Pseudo Code Using Day Event
+        //todayArray = newArrayList<>( DayEvent(Today's Date) )
+
         //Create adapter
+        //NOTICE - Again, this is set as String. May need to change.
         ArrayAdapter<String> todayAdapter = new ArrayAdapter<>(getApplicationContext(), R.layout.row_item, R.id.listItem, todayArray);
         todayList.setAdapter(todayAdapter);
         todayList.setEmptyView(findViewById(R.id.todayEmpty));
 
-        
         todayList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(MainActivity.this, DetailedDisplay.class);
-                String pass1 = "Passed from Today";
-                intent.putExtra("pass", pass1);
+
+                //Get item that is selected and pass it
+                String todayPass = (String) todayList.getItemAtPosition(position);
+                intent.putExtra("pass", todayPass);
                 startActivity(intent);
 
             }
         });
 
-        
-        //Create ArrayList list
-        ArrayList<String> tomArray = new ArrayList<>();
 
-        tomArray.add("Test List Item");
+
+        //Create ArrayList list
+        //NOTICE - ArrayList are set as String.
+        ArrayList<String> tomArray;
+
+        ArrayList<String> tomEvents = new ArrayList<>();
+
+        tomEvents.add("Test List Item");
+
+        tomArray = new ArrayList<>(tomEvents);
+
+        //Pseudo Code Using Day Event
+        //tomArray = newArrayList<>( DayEvent(Tomorrow's Date) )
 
         //Create adapter
+        //NOTICE - Again, this is set as String.
         ArrayAdapter<String> tomAdapter = new ArrayAdapter<>(getApplicationContext(), R.layout.row_item, R.id.listItem, tomArray);
         tomorrowList.setAdapter(tomAdapter);
         tomorrowList.setEmptyView(findViewById(R.id.tomEmpty));
@@ -91,18 +139,29 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(MainActivity.this, DetailedDisplay.class);
-                String pass2 = "Passed from Tomorrow";
-                intent.putExtra("pass", pass2);
+
+                //Get item that is selected and pass it
+                String tomPass = (String) tomorrowList.getItemAtPosition(position);
+                intent.putExtra("pass", tomPass);
                 startActivity(intent);
             }
         });
 
         //Create ArrayList list
-        ArrayList<String> overArray = new ArrayList<>();
+        //NOTICE - ArrayList are set as String.
+        ArrayList<String> overArray;
 
-        overArray.add("Test List Item");
+        ArrayList<String> overEvents = new ArrayList<>();
+
+        overEvents.add("Test List Item");
+
+        overArray = new ArrayList<>(overEvents);
+
+        //Pseudo Code Using Day Event
+        //overArray = newArrayList<>( DayEvent(Overmorrow's Date) )
 
         //Create adapter
+        //NOTICE - Again, this is set as String.
         ArrayAdapter<String> overAdapter = new ArrayAdapter<>(getApplicationContext(), R.layout.row_item, R.id.listItem, overArray);
         overmorrowList.setAdapter(overAdapter);
         overmorrowList.setEmptyView(findViewById(R.id.overEmpty));
@@ -112,11 +171,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(MainActivity.this, DetailedDisplay.class);
-                String pass3 = "Passed from Overmorrow";
-                intent.putExtra("pass", pass3);
+
+                //Get item that is selected and pass it
+                String overPass = (String) overmorrowList.getItemAtPosition(position);
+                intent.putExtra("pass", overPass);
                 startActivity(intent);
             }
         });
+
+        //LIZZIE - CHANGES FROM 4/30/2020 END HERE -------------------------------------------------
 
 
         goToCalen.setOnClickListener(new View.OnClickListener()
@@ -145,9 +208,22 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.home) {
+
             return true;
+        }
+
+        if (id == R.id.ad_event) {
+            Intent intent = new Intent(getApplicationContext(), AddEvent.class);
+            startActivity(intent);
+            return true;
+        }
+        //do nothing if notification settings is selected
+        if (id == R.id.no_settings) {
+            Intent intent = new Intent(getApplicationContext(), NotificationSettings.class);
+            return true;
+
+
         }
 
         return super.onOptionsItemSelected(item);
