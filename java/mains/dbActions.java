89ecbb3aa1164f9900378a72a440f5e8a82e8655
anonymousheadlights <62854004+anonymousheadlights@users.noreply.github.com>
2020-05-03 14:13:14 -0400

    Update dbActions.java

--- java/mains/dbActions.java
+++ java/mains/dbActions.java
@@ -1,18 +1,12 @@
 package com.example.alfie_s_app;
 
 import android.content.Context;
-import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,24 +17,16 @@ public class dbActions {
 
     //WRITES
     static class submit {
-        submit(final FirebaseFirestore db, EditText name, EditText building, EditText room, EditText time, EditText description, EditText date) {
-
-            //make strings
-            String sname = name.getText().toString();
-            String sbuilding = building.getText().toString();
-            String sroom = room.getText().toString();
-            String stime = time.getText().toString();
-            String sdescription = description.getText().toString();
-            String sdate = date.getText().toString();
-
+        submit(final FirebaseFirestore db, String name, String building, String room, Long time,
+               String date, String description) {
             //create map
-            Map<String, String> event = new HashMap<>();
-            event.put("title", sname);
-            event.put("building", sbuilding);
-            event.put("room", sroom);
-            event.put("time", stime);
-            event.put("description", sdescription);
-            event.put("date", sdate);
+            Map<String, Object> event = new HashMap<>();
+            event.put("title", name);
+            event.put("building", building);
+            event.put("room", room);
+            event.put("time", time);
+            event.put("description", description);
+            event.put("date", date);
 
             //put map
             db.collection("Events").add(event);
@@ -48,44 +34,42 @@ public class dbActions {
     }//end of submit
 
     public static class edit {
-        public edit(final FirebaseFirestore db, EditText name, EditText building, EditText room, EditText time, EditText description, EditText date, String docID) {
-
-            //make strings
-            String ename = name.getText().toString();
-            String ebuilding = building.getText().toString();
-            String eroom = room.getText().toString();
-            String etime = time.getText().toString();
-            String edescription = description.getText().toString();
-            String edate = date.getText().toString();
+        public edit(final FirebaseFirestore db, String name, String building, String room,
+                    long time, String description, String date, String docID) {
 
             //edit
-            if (!ename.equals("")) {
-                db.collection("Events").document(docID).update("title", ename);
+            if (!name.equals("")) {
+                db.collection("Events").document(docID)
+                        .update("title", name);
             }
-            if (!ebuilding.equals("")) {
-                db.collection("Events").document(docID).update("building", ebuilding);
+            if (!building.equals("")) {
+                db.collection("Events").document(docID)
+                        .update("building", building);
             }
-            if (!eroom.equals("")) {
-                db.collection("Events").document(docID).update("room", eroom);
+            if (!room.equals("")) {
+                db.collection("Events").document(docID)
+                        .update("room", room);
             }
-            if (!etime.equals("")) {
-                db.collection("Events").document(docID).update("time", etime);
+            //FIXME database change to long variable
+            if (time != 0) {
+                db.collection("Events").document(docID)
+                        .update("time", time);
             }
-            if (!edescription.equals("")) {
-                db.collection("Events").document(docID).update("description", edescription);
+            if (!description.equals("")) {
+                db.collection("Events").document(docID)
+                        .update("description", description);
             }
-            if (!edate.equals("")) {
-                db.collection("Events").document(docID).update("date", edate);
+            if (!date.equals("")) {
+                db.collection("Events").document(docID)
+                        .update("date", date);
             }
         }
     } //end of edit
 
     public static class delete {
-        public delete(final FirebaseFirestore db, TextView docID, final Context context) {
+        public delete(final FirebaseFirestore db, String docID, final Context context) {
 
-            String ddocID = docID.getText().toString();
-
-            db.collection("Events").document(ddocID).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+            db.collection("Events").document(docID).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
                 @Override
                 public void onSuccess(Void aVoid) {
                     Toast.makeText(context, "Deleted", Toast.LENGTH_SHORT).show();
@@ -98,37 +82,5 @@ public class dbActions {
             });
         }
     }//end of delete
-    
-    //READS
-    public static class readAll {
-        public readAll(final CollectionReference colRef, final ArrayList<Event> allData) {
-            colRef.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                @Override
-                public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                    for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
-                        Event event = documentSnapshot.toObject(Event.class); //maybe it's not actually creating an Event object????
-                        event.setDocID(documentSnapshot.getId());
-                        allData.add(event);
-                    }
-                }
-            });
-        }
-    }//end of readall
 
-
-    public static class dateSearch {
-        public dateSearch(final CollectionReference colRef, final ArrayList<Event> dateSearch, String date) {
-            colRef.whereEqualTo("date", date).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-                @Override
-                public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                    for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
-                        Event event = documentSnapshot.toObject(Event.class);
-                        event.setDocID(documentSnapshot.getId());
-                        dateSearch.add(event);
-                    }
-                }
-            });
-        }
-
-    }//end of dateSearch
 }
