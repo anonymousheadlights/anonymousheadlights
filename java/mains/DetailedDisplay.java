cb3a2da4973ce03d618c7327e230b7f43f809765
anonymousheadlights <62854004+anonymousheadlights@users.noreply.github.com>
2020-05-03 12:50:44 -0400

    Update DetailedDisplay.java

--- java/mains/DetailedDisplay.java
+++ java/mains/DetailedDisplay.java
@@ -1,14 +1,27 @@
-package com.example.alf;
+package com.example.alfie_s_app;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Map;
 
 
 public class DetailedDisplay extends AppCompatActivity {
@@ -16,11 +29,9 @@ public class DetailedDisplay extends AppCompatActivity {
     Button edit;
     Button delete;
 
-    TextView name;
-    TextView building;
-    TextView room;
-    TextView time;
-    TextView desc;
+    TextView name, building, room, date, time, desc;
+
+    final FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,35 +46,55 @@ public class DetailedDisplay extends AppCompatActivity {
         name = findViewById(R.id.cal_list_name);
         building = findViewById(R.id.cal_list_building);
         room = findViewById(R.id.cal_list_room);
+        date = findViewById(R.id.cal_list_date);
         time = findViewById(R.id.cal_list_time);
         desc = findViewById(R.id.cal_list_desc);
 
         //Test Getting that Passed Value
-        String passValue;
+        final String id;
+        String idPosition;
 
-        //Sets HomeIntent Equal to Intent Passed by Calling Procedure
-        Intent homeIntent = getIntent();
+        //Sets incomingIntent Equal to Intent Passed by Calling Procedure
+        Intent incomingIntent = getIntent();
 
         //Set String Equal to Passed Value
-        passValue = homeIntent.getStringExtra("pass");
+        idPosition = incomingIntent.getStringExtra("position");
+        id = incomingIntent.getStringExtra(idPosition);
 
         //Set TextView to Passed Value
-        name.setText(passValue);
+
+        DocumentReference docRef = db.collection("Events").document(id);
+        docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Event event = documentSnapshot.toObject(Event.class);
+                name.setText(event.getTitle());
+                building.setText(event.getBuilding());
+                room.setText(event.getRoom());
+                //FIXME formatting
+                date.setText(event.getDate());
+                //FIXME formatting
+                time.setText(event.getTime().toString());
+                desc.setText(event.getDescription());
+
+            }
+        });
 
         edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v1) {
                 Intent intent = new Intent(getApplicationContext(), EditDetailed.class);
+                intent.putExtra("id", id);
                 startActivity(intent);
-                //how are we transferring/getting data??
-
             }
         });
         delete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v2) {
-
-                //imma delete stuff here.... somehow
+                Event event = new Event(getApplicationContext(), id);
+                event.deleteEvent();
+                Intent intent = new Intent(getApplicationContext(), HomePage.class);
+                startActivity(intent);
             }
         });
 
@@ -72,9 +103,14 @@ public class DetailedDisplay extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu );
 
-
+        //make sure menu buttons are properly visible
+        MenuItem homeMenu, addMenu;
+        homeMenu = menu.findItem(R.id.home);
+        addMenu = menu.findItem(R.id.ad_event);
+        homeMenu.setVisible(true);
+        addMenu.setVisible(true);
 
         return true;
     }
@@ -83,7 +119,16 @@ public class DetailedDisplay extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-//hi lizzie
+        switch (id) {
+            case R.id.home:
+                Intent intentHome = new Intent(getApplicationContext(), HomePage.class);
+                startActivity(intentHome);
+                return true;
+            case R.id.ad_event:
+                Intent intentAdd = new Intent(getApplicationContext(), AddEvent.class);
+                startActivity(intentAdd);
+                return true;
+        }
 
         return super.onOptionsItemSelected(item);
     }
