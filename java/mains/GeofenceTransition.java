fd6825792ffd2ed0a4738eef3a4fab106fbcb586
anonymousheadlights <62854004+anonymousheadlights@users.noreply.github.com>
2020-05-03 13:06:55 -0400

    Update GeofenceTransition.java

--- java/mains/GeofenceTransition.java
+++ java/mains/GeofenceTransition.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.app.IntentService;
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
@@ -9,6 +10,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import androidx.core.app.NotificationCompat;
+
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -24,6 +26,10 @@ public class GeofenceTransition extends IntentService {
     private static final String TAG = GeofenceTransition.class.getSimpleName();
 
     public static final int GEOFENCE_NOTIFICATION_ID = 0;
+    private static final String CHANNEL_ID = "event_notif";
+    private NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, CHANNEL_ID);
+    private List<Geofence> geofenceList;
+
 
     public GeofenceTransition() {
         super(TAG);
@@ -41,10 +47,10 @@ public class GeofenceTransition extends IntentService {
 
         int geoFenceTransition = geofencingEvent.getGeofenceTransition();
         // Check if the transition type is of interest
-        if ( geoFenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ||
-                geoFenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT ) {
+        if ( geoFenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER ) {
             // Get the geofence that were triggered
             List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
+            geofenceList = triggeringGeofences;
 
             String geofenceTransitionDetails = getGeofenceTransitionDetails(geoFenceTransition, triggeringGeofences );
 
@@ -62,9 +68,7 @@ public class GeofenceTransition extends IntentService {
 
         String status = null;
         if ( geoFenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER )
-            status = "Entering ";
-        else if ( geoFenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT )
-            status = "Exiting ";
+            status = "You are close to ";
         return status + TextUtils.join( ", ", triggeringGeofencesList);
     }
 
@@ -72,12 +76,12 @@ public class GeofenceTransition extends IntentService {
         Log.i(TAG, "sendNotification: " + msg );
 
         // Intent to start the main Activity
-        Intent notificationIntent = GeofenceLocation.makeNotificationIntent(
+        Intent notificationIntent = GeofenceService.makeNotificationIntent(
                 getApplicationContext(), msg
         );
 
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-        stackBuilder.addParentStack(GeofenceLocation.class);
+        stackBuilder.addParentStack(MainActivity.class);
         stackBuilder.addNextIntent(notificationIntent);
         PendingIntent notificationPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
 
@@ -85,23 +89,43 @@ public class GeofenceTransition extends IntentService {
         // Creating and sending Notification
         NotificationManager notificatioMng =
                 (NotificationManager) getSystemService( Context.NOTIFICATION_SERVICE );
+
+        if (android.os.Build.VERSION. SDK_INT >= android.os.Build.VERSION_CODES. O ) {
+            int importance = NotificationManager. IMPORTANCE_HIGH ;
+            NotificationChannel notificationChannel = new
+                    NotificationChannel( CHANNEL_ID , "Notification Service" , importance) ;
+            notificationChannel.enableLights( true ) ;
+            notificationChannel.setLightColor(Color. RED );
+            notificationChannel.enableVibration( true ) ;
+            notificationChannel.setVibrationPattern( new long []{ 100 , 200 , 300 , 400 , 500 , 400 , 300 , 200 , 400 }) ;
+            notificationBuilder.setChannelId( CHANNEL_ID ) ;
+            assert notificatioMng != null;
+            notificatioMng.createNotificationChannel(notificationChannel) ;
+        }
+        assert notificatioMng != null;
+
         notificatioMng.notify(
                 GEOFENCE_NOTIFICATION_ID,
                 createNotification(msg, notificationPendingIntent));
-
     }
 
     // Create notification
     private Notification createNotification(String msg, PendingIntent notificationPendingIntent) {
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
-        notificationBuilder
-                .setSmallIcon(R.drawable.anonheadlight)
-                .setColor(Color.RED)
-                .setContentTitle(msg)
-                .setContentText("You hae entered a geofence!")
-                .setContentIntent(notificationPendingIntent)
-                .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_VIBRATE | Notification.DEFAULT_SOUND)
-                .setAutoCancel(true);
+        String triggeredGeofence;
+
+        for ( Geofence geofence : geofenceList ) {
+            triggeredGeofence = geofence.getRequestId();
+
+            notificationBuilder
+                    .setSmallIcon(R.drawable.anonheadlight)
+                    .setColor(Color.RED)
+                    .setContentTitle(msg)
+                    .setContentText("There are X event(s) at" + triggeredGeofence + "in the next 24 hours")
+                    .setContentIntent(notificationPendingIntent)
+                    .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_VIBRATE | Notification.DEFAULT_SOUND)
+                    .setAutoCancel(true);
+        }
+
         return notificationBuilder.build();
     }
 
