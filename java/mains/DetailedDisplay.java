7a828a5a7c5009dc3a205356edaab2e6c3aa8159
anonymousheadlights <62854004+anonymousheadlights@users.noreply.github.com>
2020-05-03 14:11:29 -0400

    Update DetailedDisplay.java

--- java/mains/DetailedDisplay.java
+++ java/mains/DetailedDisplay.java
@@ -2,26 +2,19 @@ package com.example.alfie_s_app;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.Map;
 
 
 public class DetailedDisplay extends AppCompatActivity {
